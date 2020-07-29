4fefc6d6e71e469e77dcc1f82301e192be0229b0
Vineeth <vineethcode33@outlook.com>
2020-07-28 19:20:20 -0700

    feat: added active section

--- js/app.js
+++ js/app.js
@@ -17,16 +17,34 @@
  * Define Global Variables
  *
  */
+
 let sections = document.querySelectorAll("section");
 let navUl = document.querySelector("#navbar__list");
+
 /**
  * End Global Variables
  * Start Helper Functions
  *
  */
 
+const handleScroll = () => {
+  console.log(event.target);
+};
+
 const handleNavClick = () => {
-  console.log(event);
+  let sectionClicked = event.target.dataset.nav;
+  document
+    .querySelector(`#${sectionClicked}`)
+    .scrollIntoView({ behavior: "smooth", block: "start" });
+  setNewActiveSection(sectionClicked);
+};
+
+const setNewActiveSection = (activeSection) => {
+  let oldActiveSection = document.querySelector(".your-active-class");
+  let newActiveSection = document.querySelector(`#${activeSection}`);
+
+  oldActiveSection.classList.remove("your-active-class");
+  newActiveSection.classList.add("your-active-class");
 };
 
 /**
@@ -39,14 +57,18 @@ const handleNavClick = () => {
 const addNav = () => {
   sections.forEach((element, index) => {
     let listItem = document.createElement("li");
-    listItem.setAttribute("class", "menu__link");
-    listItem.setAttribute("href", `index.html#section${index + 1}`);
-    listItem.appendChild(document.createTextNode(`Section ${index + 1}`));
+    listItem.className = "menu__link";
+    listItem.dataset.nav = `section${index + 1}`;
+    listItem.href = `index.html#section${index + 1}`;
+    listItem.innerText = `Section ${index + 1}`;
     navUl.appendChild(listItem);
   });
 };
 
 // Add class 'active' to section when near top of viewport
+const setActive = () => {
+  document.addEventListener("scroll", handleScroll);
+};
 
 // Scroll to anchor ID using scrollTO event
 const scrollToLink = () => {
@@ -64,4 +86,6 @@ addNav();
 
 // Scroll to section on link click
 scrollToLink();
+
 // Set sections as active
+setActive();
