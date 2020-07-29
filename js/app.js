c401113e1ab4cddbbe18891ebbf367fb3cb98b0c
vineethcode33 <vineethcode@outlook.com>
2020-07-28 22:39:42 -0700

    feat: aaded nav highlight on scroll

--- js/app.js
+++ js/app.js
@@ -65,8 +65,19 @@ const setNewActiveSection = (activeSection) => {
   let oldActiveSection = document.querySelector(".your-active-class");
   let newActiveSection = document.querySelector(`#${activeSection}`);
 
+  let oldNav = document.getElementById(`${oldActiveSection.id}_navlink`);
+  let newNav = document.getElementById(`${newActiveSection.id}_navlink`);
+
   oldActiveSection.classList.remove("your-active-class");
   newActiveSection.classList.add("your-active-class");
+
+  // oldNav.classList.remove("nav__active");
+  // newNav.classList.add("nav__active");
+  oldNav.style.backgroundColor = "#fff";
+  oldNav.style.color = "#333";
+
+  newNav.style.backgroundColor = "#333";
+  newNav.style.color = "#fff";
 };
 
 /**
@@ -79,9 +90,11 @@ const setNewActiveSection = (activeSection) => {
 const addNav = () => {
   sections.forEach((element, index) => {
     let listItem = document.createElement("li");
-    listItem.className = "menu__link";
-    listItem.dataset.nav = `section${index + 1}`;
-    listItem.href = `index.html#section${index + 1}`;
+    listItem.classList.add("menu__link");
+
+    listItem.id = `${element.id}_navlink`;
+    listItem.dataset.nav = `${element.id}`;
+    listItem.href = `index.html#${element.id}`;
     listItem.innerText = `Section ${index + 1}`;
     navUl.appendChild(listItem);
   });
